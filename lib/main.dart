00ef33773978d200407da5c0cbcd296216fa6d14
Damir <kalimaga12@gmail.com>
2026-04-24 13:23:17 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:bridle/game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(body: Center(child: Title ('A', HitType.hit))),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title(this.letter, this.hitType, {super.key});
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
